9fb1b538e8aeb33612ec5e2c1561421ce65206b9
Jeffrey0418 <blueskyzzz0418@gmail.com>
2023-08-10 10:38:42 +0800

    list实现20%

--- MyTinySTL/list.h
+++ MyTinySTL/list.h
@@ -17,6 +17,7 @@
 
 #include <initializer_list>
 
+#include "allocator.h"
 #include "iterator.h"
 #include "memory.h"
 // #include "functional.h"
@@ -86,6 +87,227 @@ struct ListIterator : public mystl::Iterator<mystl::BidirectionalIteratorTag, T>
   explicit ListIterator(base_ptr x) : node_(x) {}
   explicit ListIterator(node_ptr x) : node_(x->as_base()) {}
   ListIterator(const ListIterator& rhs) : node_(rhs.node_) {}
+
+  reference operator*() const { return node_->as_node()->value; }
+  pointer operator->() const { return &(operator*()); }
+
+  // 前缀调用
+  self& operator++() {
+    MYSTL_DEBUG(node_ != nullptr);
+    node_ = node_->next;
+    return *this;
+  }
+
+  // 后缀调用
+  self operator++(int) {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  self& operator--() {
+    MYSTL_DEBUG(node_ != nullptr);
+    node_ = node_->prev;
+    return *this;
+  }
+
+  self operator--(int) {
+    self tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  bool operator==(const self& rhs) const { return node_ == rhs.node_; }
+  bool operator!=(const self& rhs) const { return node_ != rhs.node_; }
+};
+
+template <typename T>
+struct ListConstIterator : public Iterator<BidirectionalIteratorTag, T> {
+  using value_type = T;
+  using pointer = const T*;
+  using reference = const T&;
+  using base_ptr = typename NodeTraits<T>::base_ptr;
+  using node_ptr = typename NodeTraits<T>::node_ptr;
+  using self = ListConstIterator<T>;
+
+  base_ptr node_;
+
+  ListConstIterator() = default;
+  explicit ListConstIterator(base_ptr x) : node_(x) {}
+  explicit ListConstIterator(node_ptr x) : node_(x->as_base()) {}
+  explicit ListConstIterator(const ListIterator<T>& rhs) : node_(rhs.node_) {}
+  ListConstIterator(const ListConstIterator& rhs) : node_(rhs.node_) {}
+
+  reference operator*() const { return node_->as_node()->value; }
+  pointer operator->() const { return &(operator*()); }
+
+  self& operator++() {
+    MYSTL_DEBUG(node_ != nullptr);
+    node_ = node_->next;
+    return *this;
+  }
+
+  self operator++(int) {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  self& operator--() {
+    MYSTL_DEBUG(node_ != nullptr);
+    node_ = node_->prev;
+    return *this;
+  }
+
+  self operator--(int) {
+    self tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  bool operator==(const self& rhs) const { return node_ == rhs.node_; }
+  bool operator!=(const self& rhs) const { return node_ != rhs.node_; }
+};
+
+template <typename T>
+class List {
+ public:
+  using allocator_type = mystl::Allocator<T>;
+  using data_allocator = mystl::Allocator<T>;
+  using base_allocator = mystl::Allocator<ListNodeBase<T>>;
+  using node_allocator = mystl::Allocator<ListNode<T>>;
+
+  using value_type = typename allocator_type::value_type;
+  using pointer = typename allocator_type::poninter;
+  using const_pointer = typename allocator_type::const_pointer;
+  using reference = typename allocator_type::reference;
+  using const_reference = typename allocator_type::const_reference;
+  using size_type = typename allocator_type::size_type;
+  using difference_type = typename allocator_type::difference_type;
+
+  using iterator = ListIterator<T>;
+  using const_iterator = ListConstIterator<T>;
+  using reverse_iterator = mystl::ReverseIterator<iterator>;
+  using const_reverse_iterator = mystl::ReverseIterator<const_iterator>;
+
+  using base_ptr = typename NodeTraits<T>::base_ptr;
+  using node_ptr = typename NodeTraits<T>::node_ptr;
+
+  allocator_type get_allocator() { return node_allocator(); }
+
+ private:
+  base_ptr node_;   // 指向末尾结点
+  size_type size_;  // 大小
+
+ public:
+  // 构造、复制、移动、析构函数
+  List() { fill_init(0, value_type()); }
+
+  explicit List(size_type n) { fill_init(n, value_type()); }
+
+  List(size_type n, const T& value) { fill_init(n, value); }
+
+  template <
+      typename Iter,
+      typename std::enable_if<mystl::IsInputIterator<Iter>::kValue, int>::type = 0>
+  List(Iter first, Iter last) {
+    copy_init(first, last);
+  }
+
+  List(std::initializer_list<T> ilist) { copy_init(ilist.begin(), ilist.end()); }
+
+  List(const List& rhs) { copy_init(rhs.cbegin(), rhs.cend()); }
+
+  List(List&& rhs) noexcept : node_(rhs.node_), size_(rhs.size_) {
+    rhs.node_ = nullptr;
+    rhs.size_ = 0;
+  }
+
+  List& operator=(const List& rhs) {
+    if (this != &rhs) {
+      assign(rhs.begin(), rhs.end());
+    }
+    return *this;
+  }
+
+  List& operator=(List&& rhs) noexcept {
+    clear();
+    splice(end(), rhs);
+    return *this;
+  }
+
+  List& operator=(std::initializer_list<T> ilist) {
+    List tmp = (ilist.begin(), ilist.end());
+    swap(tmp);
+    return *this;
+  }
+
+  ~List() {
+    if (node_) {
+      clear();
+      base_allocator::deallocate(node_);
+      node_ = nullptr;
+      size_ = 0;
+    }
+  }
+
+ public:
+  // 迭代器相关操作
+  iterator begin() noexcept { return node_->next; }
+
+  const_iterator begin() const noexcept { return node_->next; }
+
+  iterator end() noexcept { return node_; }
+
+  const_iterator end() const noexcept { return node_; }
+
+  reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+
+  const_reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
+
+  reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+
+  const_reverse_iterator rend() const noexcept { return reverse_iterator(begin()); }
+
+  const_iterator cbegin() const noexcept { return begin(); }
+
+  const_iterator cend() const noexcept { return end(); }
+
+  const_reverse_iterator crbegin() const noexcept { return rbegin(); }
+
+  const_reverse_iterator crend() const noexcept { return rend(); }
+
+  // 容量相关操作
+  bool empty() const noexcept { return node_->next == node_; }
+
+  size_type size() const noexcept { return size_; }
+
+  size_type max_size() const noexcept { return static_cast<size_type>(-1); }
+
+  // 访问元素相关操作
+  reference front() {
+    MYSTL_DEBUG(!empty());
+    return *begin();
+  }
+
+  const_reference front() const {
+    MYSTL_DEBUG(!empty());
+    return *begin();
+  }
+
+  reference back() {
+    MYSTL_DEBUG(!empty());
+    return *(--end());
+  }
+
+  const_reference back() const {
+    MYSTL_DEBUG(!empty());
+    return *(--end());
+  }
+
+  // 调整容器相关操作
+  // assign
+  void assign(size_type)
 };
 
 }  // namespace mystl
